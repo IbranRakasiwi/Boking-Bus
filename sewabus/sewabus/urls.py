83beb3314fc8f58652b44031443147028748c60c
IbranRakasiwi <ibranrakasiwimd22@gmail.com>
2020-11-20 14:42:32 +0700

    sewa bus

--- sewabus/sewabus/urls.py
+++ sewabus/sewabus/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    # path('/', include('login.urls')),
+    path('sewa/', include('sewa.urls')),
 
 
 ]
