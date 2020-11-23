b90d9bc279ad6cbb9d7c9423aff0105afb4ef5b6
IbranRakasiwi <ibranrakasiwimd22@gmail.com>
2020-11-23 16:02:22 +0700

    sewa bus

--- sewabus/sewa/urls.py
+++ sewabus/sewa/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib import admin
 from django.shortcuts import render
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index),
--- sewabus/sewabus/settings.py
+++ sewabus/sewabus/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'sewabus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,4 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
