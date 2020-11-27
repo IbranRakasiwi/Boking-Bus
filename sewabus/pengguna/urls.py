e12c5e790f398cb5572c6a2cdd7490c2cf114d7b
IbranRakasiwi <ibranrakasiwimd22@gmail.com>
2020-11-27 18:00:08 +0700

    kelompok

--- sewabus/pengguna/urls.py
+++ sewabus/pengguna/urls.py
@@ -1,12 +1,16 @@
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.shortcuts import render
 from . import views
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.tampil),
-    path('tampilmurid/', views.tampilmurid, name='murid'),
-    path('tampilguru/', views.tampilguru, name='pengajar'),
-    path('detailguru/', views.detailguru, name='gdetail'),
-    path('detailmurid/', views.detailmurid, name='mdetail'),
+    path('user/', views.user, name='user'),
+    path('tabel/', views.tabel, name='tabel'),
+    # path('tampilguru/', views.tampilguru, name='pengajar'),
+    # path('detailguru/', views.detailguru, name='gdetail'),
+    # path('detailmurid/', views.detailmurid, name='mdetail'),
 
 ]
--- sewabus/pengguna/views.py
+++ sewabus/pengguna/views.py
@@ -9,39 +9,44 @@ from django.contrib import messages
 def tampil(request):
     return render(request, 'pengguna/index.html')
 
-def murid(request):
-    return render(request, 'murid.html')
+def user(request):
+   
+    return render(request, 'pengguna/user.html')
+
+def tabel (request):
+   
+    return render(request, 'pengguna/tabel.html')
 
 # PENGAJAR
 
-def tampilguru(request):
-    if request.POST:
-        models.pengajar.objects.all()
+# def tampilguru(request):
+#     if request.POST:
+#         models.pengajar.objects.all()
         
-    ptampil = models.pengajar.objects.all()
-    return render(request, 'pengajar.html',
-		{ 'data': ptampil,
-		})
+#     ptampil = models.pengajar.objects.all()
+#     return render(request, 'pengajar.html',
+# 		{ 'data': ptampil,
+# 		})
 
-def detailguru(request, id):
-	gdetail = models.pengajar.objects.filter(pk=id).first()
-	return render(request, 'detailpengajar.html',
-		{ 'data': gdetail,
-		})
+# def detailguru(request, id):
+# 	gdetail = models.pengajar.objects.filter(pk=id).first()
+# 	return render(request, 'detailpengajar.html',
+# 		{ 'data': gdetail,
+# 		})
 
-# MURID
+# # MURID
 
-def tampilmurid(request):
-    if request.POST:
-        models.murid.objects.all()
+# def tampilmurid(request):
+#     if request.POST:
+#         models.murid.objects.all()
         
-    mtampil = models.murid.objects.all()
-    return render(request, 'murid.html',
-		{ 'data': mtampil,
-		})
+#     mtampil = models.murid.objects.all()
+#     return render(request, 'murid.html',
+# 		{ 'data': mtampil,
+# 		})
 
-def detailmurid(request, id):
-	mdetail = models.murid.objects.filter(pk=id).first()
-	return render(request, 'detailmurid.html',
-		{ 'data': mdetail,
-		})
+# def detailmurid(request, id):
+# 	mdetail = models.murid.objects.filter(pk=id).first()
+# 	return render(request, 'detailmurid.html',
+# 		{ 'data': mdetail,
+# 		})
